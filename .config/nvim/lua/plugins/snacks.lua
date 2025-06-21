697e165a45db44b4109c4bfd524dcfb0c7b99e1c
dogukanyesilkaya <dogukanyesilkaya@outlook.com>
2025-06-22 02:09:12 +0300

    tmux,nvim and zshrc

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,25 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = false },
+    dashboard = { enabled = true },
+    explorer = { enabled = false },
+
+    lazygit = { enabled = true },
+    indent = { enabled = false },
+    input = { enabled = false },
+    picker = { enabled = false },
+    notifier = { enabled = true },
+    quickfile = { enabled = false },
+    scope = { enabled = true },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = true },
+    words = { enabled = false },
+  },
+}
