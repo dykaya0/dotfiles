5d00919f0d4270fddf2587403c38eb0ec2dc2318
dogukanyesilkaya <dogukanyesilkaya@outlook.com>
2025-08-03 20:20:56 +0300

    hyprland stow fixed

--- .config/hypr/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Launch the special workspace and Ghostty
-# hyprctl dispatch togglespecialworkspace terminal
-# sleep 1
-# ghostty
-# sleep 1
-# hyprctl dispatch togglespecialworkspace terminal
